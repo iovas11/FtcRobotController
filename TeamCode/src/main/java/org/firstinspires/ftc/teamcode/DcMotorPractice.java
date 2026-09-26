79a7c083eca1d4be50659c47eadafe9c263a0578
iovas11 <ioana.vasiliu.02042011@gmail.com>
2026-09-26 15:06:50 +0300

    Create DcMotorPractice.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DcMotorPractice.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
+import org.firstinspires.ftc.teamcode.mechanisms.TestBench1;
+
+@TeleOp
+public class DcMotorPractice extends OpMode {
+    TestBench bench = new TestBench();
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+
+    }
+
+
+    @Override
+    public void loop() {
+        if (bench.isTouchSensorPressed()) {
+            bench.setMotorSpeed((0.5));
+        }
+        else {
+            bench.setMotorSpeed(0.0); //stops the motor
+        }
+        bench.setMotorSpeed(0.5);
+        telemetry.addData("Motor Revs" , bench.getMotorRevs());
+
+    }
+}
+
