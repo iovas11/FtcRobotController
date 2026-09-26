2dddb7fa4d6ec89dee8dab48692a64441544b3df
iovas11 <ioana.vasiliu.02042011@gmail.com>
2026-09-26 15:03:51 +0300

    Update TestBench.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
@@ -1,22 +1,40 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class TestBench {
+    private DcMotor motor; //linearSlideMotor0
+    private double ticksPerRev; //revolution
     private DigitalChannel touchSensor; //default name for touch sensor
 
     public void init(HardwareMap hwMap) {
+        //Touch Sensor
         touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
+        //DC motor section
+        motor = hwMap.get(DcMotor.class, "motor");
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ticksPerRev = motor.getMotorType().getTicksPerRev();
 
     }
 
+    //Touch Sensor
     public boolean isTouchSensorPressed() {
         return !touchSensor.getState();
     }
 
     public boolean isTouchSensorReleased() {
         return touchSensor.getState();
+    }
+
+    //DC Motor
+    public void setMotorSpeed(double speed) {
+        motor.setPower(speed);
+    }
+
+    public double getMotorRevs() {
+        return motor.getCurrentPosition() / ticksPerRev; //normalizing ticks to revolution
     }
 }
